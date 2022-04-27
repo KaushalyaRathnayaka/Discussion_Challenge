2179ab840abf55ec1b3b702421afe34bf513da03
nchathu2014 <nchathu2014@gmail.com>
2022-04-27 09:10:28 +0530

    Update reverseString.js

--- Discussion_challenge1/reverseString.js
+++ Discussion_challenge1/reverseString.js
@@ -2,6 +2,12 @@
 
 function reverseString(str) {
 
+    /*
+    1 - Congratulations 👏👏👏✔, it is working
+2 - try to do some input validations, if user add a number instead of a string then how you handle it
+3- Try to do it without let empty_str, instead of creating a auxiliary variable you can swap the elements with in the same array
+    */
+    
     // empty string
     let empty_str = "";
     for (let i = str.length - 1; i >= 0; i--) {
