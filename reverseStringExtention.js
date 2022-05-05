13d37efbcdb1d76a84779ded6be8c1b87762d448
KaushalyaRathnayaka <88085501+KaushalyaRathnayaka@users.noreply.github.com>
2022-05-05 09:08:18 +0530

    Add reverseStringExtention.js file

---
+++ reverseStringExtention.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+    var splitString = str.split("");
+  
+    var reverseArray = splitString.reverse();
+  
+    var joinArray = reverseArray.join("");
+  
+    return joinArray;
+  }
+  
+  console.log(reverseString("hello"));
